777b0d327faad031e25bff8804fa5f83ead9798e
Arvindkr123 <thakurarvindkr10@gmail.com>
2026-01-07 03:24:56 +0530

    added conflict with same route and resolve

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ def index():
     return {
         'data':'blog list'
     }
-    
-@app.get("/blog/{id}")
-def about(id):
+
+@app.get("/blog/unpublished")
+def unpublished():
     return {
-        'data':f'blog {id}'
+        'data':'all unpublished blogs'
     }
 
+
+@app.get("/blog/{id}")
+def about(id:int):
+    return {
+        'data':id
+    }
+    
+
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id:int):
     return {
         'data':{
             '1','2'
